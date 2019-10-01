af6add7762d8b0ee6a017ac321685b0e38c7b1a8
Sebastian Cerpa <you@example.com>
2019-09-30 21:08:05 -0300

    Se incorpora funcionalidad a PRODUCTO INTERNO

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-
-# Program to Show how to create a switch  
-# import kivy module     
 import kivy   
 import random
 import numpy as np  
 import matplotlib.pyplot as mat
 
-# base Class of your App inherits from the App class.     
-# app:always refers to the instance of your application
-
 from kivy.app import App  
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 
-# this restrict the kivy version i.e   
-# below this kivy version you cannot   
-# use the app or software   
 kivy.require('1.9.0') 
   
-# Builder is used when .kv file is 
-# to be used in .py file 
-from kivy.lang import Builder 
-  
-# The screen manager is a widget 
-# dedicated to managing multiple screens for your application. 
+from kivy.lang import Builder   
 from kivy.uix.screenmanager import ScreenManager, Screen 
    
-# You can create your kv code in the Python file 
 Builder.load_file('main.kv')
-   
-# Create a class for all screens in which you can include 
-# helpful methods specific to that screen
 
 class Ventana_Menu(Screen): 
     pass
@@ -43,17 +25,17 @@ class EFOptionWindow(Screen):
   
 class EUNormType(Screen, Widget):
     def inputN(self):
-        #Taking values front the input to the codeb
+        
         nParsed = int(self.n.text)
         pParsed = int(self.p.text)
         menor_valor = -500
         mayor_valor = 500
-        #Random array
+ 
         arr = np.zeros(nParsed)
+        
         for i in range(nParsed):
             valor_aleatorio = random.randint(menor_valor,mayor_valor)
             arr[i] = valor_aleatorio
-        print(arr)
 
         # TIPOS DE NORMA
         # |X|1
@@ -80,19 +62,21 @@ class ScreenFive(Screen):
    
 class Producto_Vectorial (Screen, Widget):
     def inputN(self):
-        #Taking values front the input to the codeb
+
         x1Parsed = int(self.x1.text)
         x2Parsed = int(self.x2.text)
         x3Parsed = int(self.x3.text)
         y1Parsed = int(self.y1.text)
         y2Parsed = int(self.y2.text)
         y3Parsed = int(self.y3.text)
+        
         vector_1=[x1Parsed,x2Parsed,x3Parsed]
         vector_2=[y1Parsed,y2Parsed,y3Parsed]
-        print(vector_1)
+        
         sub_mat_i=([x2Parsed,x3Parsed],[y2Parsed,y3Parsed])
         sub_mat_j=([x1Parsed,x3Parsed],[y1Parsed,y3Parsed])
         sub_mat_k=([x1Parsed,x2Parsed],[y1Parsed,y2Parsed])
+        
         i=(np.linalg.det(sub_mat_i))*1
         j=(np.linalg.det(sub_mat_j))*-1
         k=(np.linalg.det(sub_mat_k))*1
@@ -108,31 +92,34 @@ class Grafico_N_R2 (Screen):
 	pass
 
 class Producto_Interno (Screen, Widget):
-        pass
-    #def inputN(self):
-        #cantidad_vectores=3
-        #vector= np.zeros(n)
-        #arreglo_vectores = np.zeros((cantidad_vectores,n))
-        #pivote=1
-        #resultado=0
-        #for i in range(cantidad_vectores):
-        #    for j in range(n):
-	#	valor_aleatorio = random.randint(menor_valor,mayor_valor)
-	#	arreglo_vectores[i][j]=valor_aleatorio
-        #print(arreglo_vectores)
-
-        #for j in range(n):
-        #    for i in range(cantidad_vectores):
-	#	pivote=arreglo_vectores[i][j]*pivote
-         #   resultado=pivote+resultado
-         #   print("Multiplicacion")	
-        #    print(pivote)
-        #    print("Suma")
-        #    print(resultado)
-        #    pivote=1
-        #print(resultado)
+    def inputN(self):
         
-        #nParsed = int(self.n.text)
+        nParsed = int(self.n.text)
+        vector_x=np.zeros(nParsed)
+        vector_y=np.zeros(nParsed)
+        menor_valor = -10
+        mayor_valor = 10
+        resultado=0
+        
+        for i in range(nParsed):
+            valor_aleatorio = random.randint(menor_valor,mayor_valor)
+            vector_x[i]=valor_aleatorio
+
+        for i in range(nParsed):
+            valor_aleatorio = random.randint(menor_valor,mayor_valor)
+            vector_y[i]=valor_aleatorio
+
+        print(vector_x)
+        print(vector_y)
+
+        for i in range(nParsed):
+            resultado=vector_x[i]*vector_y[i]+resultado
+            print(vector_x[i]*vector_y[i])
+        print(resultado)
+        
+        self.output_vector_x.text = str(vector_x)
+        self.output_vector_y.text = str(vector_y)
+        self.output_resultado.text = str(resultado)
 
 class Producto_Mixto (Screen, Widget):
         pass
